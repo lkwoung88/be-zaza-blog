ab7f36d4d7085770399235d07bd9e125eae78fec
lkwoung88 <lkwoung88@gmail.com>
2024-08-08 01:05:50 +0900

    init project !

---
+++ src/test/java/zaza/techblog/TechblogApplicationTests.java
@@ -0,0 +1,13 @@
+package zaza.techblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
