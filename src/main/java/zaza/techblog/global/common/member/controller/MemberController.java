4524adbe363eba690be881827f19451698c0840c
lkwoung88 <lkwoung88@gmail.com>
2024-10-07 03:06:39 +0900

    [fix] request to entity - request to entity 책임 재분배

--- src/main/java/zaza/techblog/global/common/member/controller/MemberController.java
+++ src/main/java/zaza/techblog/global/common/member/controller/MemberController.java
@@ -5,26 +5,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zaza.techblog.global.common.member.request.JoinMemberRequest;
 import zaza.techblog.global.common.member.service.MemberService;
 import zaza.techblog.global.common.response.BaseResponse;
-import zaza.techblog.global.common.member.entity.Member;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
 
     @PutMapping("/join")
     public ResponseEntity<BaseResponse> joinZazaMember(@RequestBody @Valid JoinMemberRequest request, BindingResult result) {
-        BaseResponse response = memberService.joinZazaMember(new Member(request));
+        BaseResponse response = memberService.joinZazaMember(request.toEntity());
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/zaza/techblog/global/common/member/request/JoinMemberRequest.java
+++ src/main/java/zaza/techblog/global/common/member/request/JoinMemberRequest.java
@@ -3,6 +3,9 @@ package zaza.techblog.global.common.member.request;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import zaza.techblog.global.common.code.RoleCode;
+import zaza.techblog.global.common.code.StatusCode;
+import zaza.techblog.global.common.member.entity.Member;
 
 @Getter
 public class JoinMemberRequest {
@@ -24,4 +27,16 @@ public class JoinMemberRequest {
     @NotEmpty
     // TODO 정규식
     private String email;
+
+    public Member toEntity() {
+        return Member.builder()
+                .id(id)
+                .password(password)
+                .name(name)
+                .email(email)
+                .role(RoleCode.ROLE_USER)
+                .status(StatusCode.ACTIVE)
+                .oAuthProvider("")
+                .build();
+    }
 }
