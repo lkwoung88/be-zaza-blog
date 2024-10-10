d5b45acb8c439fa0e5eb8033e41d5c23a1f16196
lkwoung88 <lkwoung88@gmail.com>
2024-10-11 02:06:23 +0900

    [fix] oauth -> 멤버저장 수정 - transactional 추가

--- src/main/java/zaza/techblog/global/auth/service/SocialOAuth2UserService.java
+++ src/main/java/zaza/techblog/global/auth/service/SocialOAuth2UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import zaza.techblog.global.auth.dto.GoogleOAuthResponse;
 import zaza.techblog.global.auth.dto.NaverOAuthResponse;
 import zaza.techblog.global.auth.dto.SocialOAuthResponse;
@@ -27,6 +28,7 @@ public class SocialOAuth2UserService extends DefaultOAuth2UserService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
